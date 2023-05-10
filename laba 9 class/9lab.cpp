0568c56f1bad0a6ee061fe71256aabeec40191ed
Mike Karnaukhov <115707524+Suduppa@users.noreply.github.com>
2023-05-10 15:38:21 +0500

    Лаба 9 классы

---
+++ laba 9 class/9lab.cpp
@@ -0,0 +1,31 @@
+﻿
+#include "Vector.h"
+#include "Error.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	try
+	{
+		int size;
+		cout << "Введите количество элементов в векторе: " << endl;
+		cin >> size;
+		Vector x(size);
+		Vector y;
+		cout << "Размерность вектора: " << x() << endl; 
+		cout << "Для доступа к индексу, введите его номер: ";
+		int i;
+		cin >> i;
+		cout << x[i] << endl; 
+		--x;
+		cout << x;
+		++x;
+		cout << x;
+	}
+	catch (Error& e)
+	{
+		e.what();
+	}
+	return 0;
+}
