55f7789975a74b82c8bf7790684f26dce3e2f091
Mike Karnaukhov <115707524+Suduppa@users.noreply.github.com>
2023-05-25 19:49:01 +0500

    Laba 11 class (3)

---
+++ Laba 11 class (3)/11Lab_3.cpp
@@ -0,0 +1,24 @@
+﻿#include "Vector.h"
+#include <iostream>
+#include "Time.h"
+using namespace std;
+
+void main()
+{
+    setlocale(LC_ALL, "Russian");
+	Vector<Time>vec(2);    
+    vec.Print();       
+    Time s = vec.Srednee(); 
+    cout << "Srednee=" << s << endl;
+    cout << "pos?";
+    int p;
+    cin >> p
+    p = vec.Min();  
+    vec.Add(s, p);
+    vec.Print();
+    p = vec.Max();  
+    vec.Del(p);   
+    vec.Print();    
+    vec.Delenie();  
+    vec.Print();   
+}
